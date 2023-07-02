5089b9f34e1a47591221921b59720ad7745fb032
Salik Qayyum SP20-BCS-030 <salikqayyum437@gmail.com>
2023-07-02 14:39:52 +0500

    Notice board with firebase app
    color change & user added
    User ID: hamzacoin78@gmail.com
    Passward: 123456

--- notice_board_app/lib/main.dart
+++ notice_board_app/lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Notice Board App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.amber,
       ),
       home: LoginPage(),
     );
